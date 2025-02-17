84c5bc565a1921b9fe6b82c0db36e77e85af1fd2
Cleslley de Moura <cleslleydemoura@gmail.com>
2025-02-17 11:34:11 -0300

    feat: Criação do Controller de Stock

--- api/Program.cs
+++ api/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDBContext> (options => {
